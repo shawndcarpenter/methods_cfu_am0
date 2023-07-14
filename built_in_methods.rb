44bf5de0c6eb39190fa0d3a1cee9fe7d9adb9bb7
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-07-14 12:11:32 -0600

    Finish final_practice

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -10,23 +10,44 @@
 
 
 "Hello World".include?("Hello")
-
+# The include method is called on the string object "Hello World"
+#No arguments are passed; the include method has one clear job which is to check if the string includes the string listed in the parentheses
+# The return value is the boolean "true," as "Hello World" includes "Hello"
 
 "Hello World".end_with?("Hello")
+# The end_with method is called on the string object "Hello World"
+# No arguments are passed; the end_width method has the one clear job of checking whether the string it is called on ends with the string in the parentheses
+# The return value is the boolean "false," as "Hello World" does not end with "Hello"
 
 
 "Hello World".end_with?("rld")
-
+# The end_with method is called on the string object "Hello World"
+# No arguments are passed; the end_width method has the one clear job of checking whether the string it is called on ends with the string in the parentheses
+# The return value is the boolean "true," as "Hello World" ends with "rld"
 
 12.even?
-
+# The even method is called on the integer of 12
+# No arguments are passed; the even method has the one clear job of checking whether the integer value is even or not
+# the return value is "true," as 12 is an even integer
 
 18.next
-
+# The next method is called on the integer of 18
+# No arguments are passed; the next method has one clear job of checking what the next integer value is
+# The return value is 19, as the integer 19 comes next after 18
 
 
 # SECTION 2: Calling methods on variables assigned to strings.
 # Declare 2 variables assigned to string objects.
+name = "Shaawn"
+pet_name = "Luna"
+# The swapcase method is called on the "pet_name" variable containing the string object of "Luna".
+# No arguments are passed; this method swaps the cases of the letters within a string; uppercase letters are lowercased, and lowercase letters are uppercased
+# The return value is "lUNA," as the lower- and uppercase letters have swapped cases.
+pet_name.swapcase!
+# The squeeze method is called on the "name" variable containing the string object of "Shaawn"
+# No arguments are passed; This method removes contiguous repeating characters in a string.
+#The return value is "Shawn", as the duplicate "a" was "squeezed" out.
+name.squeeze
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/String.html
 # Include comments above each method call explaining the impact and return value of that method.
@@ -46,6 +67,22 @@ puts first_name.start_with?("J")
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
+# The remainder method is called on the number_of_birdhouses variable, which stores the integer of 12.
+# The remainder method returns the remainder left over after dividing the integer using the given defined value, in this case 4.
+# In this example, the return value is 0, because 12 is divisible by 4 with no remainder.
+# The puts command prints the return value of the remainder method (0) to the console.
+number_of_birdhouses = 12
+puts number_of_birdhouses.remainder(4)
+# The to_f method is called on the number_of_nails variable, storing an integer of 56.
+# The to_f method returns the integer value as a float.
+# In this example, the return value is 56.0, because the integer of 56 is 56.0 when converted to a float.
+# The puts command prints the return value of the to_f method to the console.
+number_of_nails = 56
+puts number_of_nails.to_f
+
+
+
+
 
 
 
@@ -54,3 +91,17 @@ puts first_name.start_with?("J")
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+# The shuffle method is called on the array of moon_phases containing 4 string elements.
+# The shuffle method will rearrange the elements within in an array in a random order.
+# In this example, the return value will be the same 4 elements listed, but in a random order, such as ["Last Quarter", "Full Moon", "New Moon", "First Quarter"]
+# The print command prints the randomized return value of ["Last Quarter", "Full Moon", "New Moon", "First Quarter"] to the console.
+moon_phases = ["New Moon", "First Quarter", "Full Moon", "Last Quarter"]
+print moon_phases.shuffle
+
+# The minmax method is called on the array of number_of_books containing 4 integer elements.
+# The minmax method returns the highest and lowest values from an array.
+# In this example the return value will be [3,27], as this represents the highest and lowest elements in the number_of_books array.
+# The print command prints [3, 27]
+number_of_books = [8, 17, 3, 27]
+print number_of_books.minmax
--- final_practice.rb
+++ final_practice.rb
@@ -7,33 +7,78 @@
 # 1: Write a method named greeting that returns a string with a general greeting. 
 
 # What is the return value of your method?
+#The return value of my method is "Hello and Welcome."
 # How many arguments did you pass your method?
-
+# There were no arguments passed for this method.
+def greeting
+    "Hello and Welcome!"
+end
+default_greet = greeting
+puts default_greet
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
-
+def custom_greeting(name)
+    "Hello and Welcome, #{name}!"
+end
+shawn_greet = custom_greeting("Shawn")
+puts shawn_greet
+dave_greet = custom_greeting("Dave")
+puts dave_greet
 # What is the return value of your method?
+# The return value of this method is "Hello and Welcome, Shawn!"
 # How many arguments did you pass your method?
+# I passed one argument.
 # What data type was your argument(s)?
-
+# The data type of this argument was a string.
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
-
+def greet_person(first, middle, last)
+    "Hello, #{first} #{middle} #{last}!"
+end
+full_name_greet1 = greet_person("Shawn", "D", "Carpenter")
+puts full_name_greet1
+full_name_greet2 = greet_person("Dave", "D", "Douglas")
+puts full_name_greet2
 # What is the return value of your method?
+# The return value of my method is "Hello, Shawn D Carpenter!"
 # How many arguments did you pass your method?
+# This method passed 3 arguments.
 # What data type was your argument(s)?
-
+# The data type of my arguments was strings.
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
-
+def square(int)
+   "The square of #{int} is #{int * int}."
+end
+square_of_2 = square(2)
+puts square_of_2
+square_of_9 = square(9)
+puts square_of_9
 # What is the return value of your method?
+# The return value of my method is "The square of 2 is 4."
 # How many arguments did you pass your method?
+# I passed one argument.
 # What data type was your argument(s)?
-
+# The data type for my argument was an integer.
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
+
+def check_stock(number_of_items, item_name)
+    if
+        number_of_items >= 4 
+        puts "#{item_name} is stocked"
+    elsif
+        number_of_items == 0 
+        puts "#{item_name} - OUT of stock!"
+    else
+        number_of_items <4 
+        puts "#{item_name} - running LOW"
+    
+    end
+end
+
 
 check_stock(4, "Coffee");
 # => "Coffee is stocked"
